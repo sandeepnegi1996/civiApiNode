4854d87972abe5be6e5f382ef2bf5a6eb4297d46
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-09-06 12:55:39 +0530

    working api

--- routes/DynamoDbRoutes/RequestCiviRoute.js
+++ routes/DynamoDbRoutes/RequestCiviRoute.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 var AWS = require("aws-sdk");
-var attr = require("dynamodb-data-types").AttributeValue;
-// Set the region
 AWS.config.update({ region: "us-east-2" });
+
 var ddb = new AWS.DynamoDB.DocumentClient();
 
 
@@ -29,12 +28,14 @@ router.post("/", (req, res) => {
     };
     var finalDataObj;
     ddb.query(params, function (err, data) {
+        console.log("inside query");
         if (err) {
             console.log("Error", err);
         } else {
             //   console.log(data);
             // console.log(data.Items[0].DeviceID);
             // console.log(data.Items[0].data);
+            console.log("inside the else block");
             console.log(data.Items[0].data);
             finalDataObj = data.Items[0].data
             console.log(finalDataObj.DeviceID);
@@ -60,12 +61,12 @@ router.post("/", (req, res) => {
         }
     });
 
-
+    //res.send("unable to request the database")
 });
 
-// router.get("/", (req, res) => {
-//     res.json("request checking dynamo db");
-// });
+router.get("/test", (req, res) => {
+    res.json("request checking dynamo db");
+});
 
 module.exports = router;
 
@@ -73,63 +74,4 @@ module.exports = router;
 
 
 
-
-
-
-// // Load the AWS SDK for Node.js
-// var AWS = require("aws-sdk");
-// var attr = require("dynamodb-data-types").AttributeValue;
-// // Set the region
-// AWS.config.update({ region: "us-east-2" });
-
-// // Create the DynamoDB service object
-// var ddb = new AWS.DynamoDB.DocumentClient();
-
-// // var params = {
-// //     TableName: "civinew",
-// //     Key: {
-// //         DeviceId: { S: "cividevice1" },
-// //         Timestamp: { S: "1567578190750" }
-// //     }
-// // };
-
-// // ddb.getItem(params, function (err, data) {
-// //     if (err) {
-// //         console.log("Error", err);
-// //     } else {
-// //         console.log(data);
-// //         // var obj = data.Item.data["M"].state["M"].reported["M"];
-// //         //console.log(obj);
-// //         // var obj = data.Item.data["M"].state["M"].reported["M"];
-// //         // console.log(obj.color);
-// //         // console.log(obj.temperature);
-// //         // console.log(obj.DeviceId);
-// //     }
-// // });
-
-// var params = {
-//     TableName: "civinew",
-//     KeyConditionExpression: "#deviceid = :deviceidvalue",
-//     ExpressionAttributeNames: {
-//         "#deviceid": "DeviceID"
-//     },
-//     ExpressionAttributeValues: {
-//         ":deviceidvalue": "cividevice1"
-//     },
-//     Limit: 1,
-//     ScanIndexForward: false
-// };
-
-
-// ddb.query(params, function (err, data) {
-//     if (err) {
-//         console.log("Error", err);
-//     } else {
-//         //   console.log(data);
-//         console.log(data.Items[0].DeviceID);
-//         console.log(data.Items[0].data);
-//         console.log(typeof data.Items[0].data);
-
-//     }
-// });
 
